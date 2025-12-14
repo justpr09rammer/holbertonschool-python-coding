3b6115764e1a4605e66d0d5ad5ea73b3e7c4302d
justpr09rammer <nihadqocayev152@gmail.com>
2025-12-14 17:10:49 +0000

    made the class executable

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,7 +3,6 @@
 This module defines a class Square with a private instance attribute 'size'.
 It allows creating a square object with a specific size.
 """
-
 class Square:
     """
     Represents a square with a private size attribute.
@@ -11,7 +10,6 @@ class Square:
     Attributes:
         __size (int): The size of the square (private).
     """
-
     def __init__(self, size):
         """
         Initializes a Square instance with a given size.
