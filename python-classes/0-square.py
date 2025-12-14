14f167e3708017e5d6653c54590d1b093506079c
justpr09rammer <nihadqocayev152@gmail.com>
2025-12-14 17:14:12 +0000

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,70 +1,9 @@
 #!/usr/bin/python3
+"""Square generation module for Python project 0x06
 """
-This module defines a class Square with a private instance attribute 'size'.
-It allows creating a square object with a specific size.
-"""
+
+
 class Square:
+    """class defined for square generation
     """
-    Represents a square with a private size attribute.
-
-    Attributes:
-        __size (int): The size of the square (private).
-    """
-    def __init__(self, size):
-        """
-        Initializes a Square instance with a given size.
-
-        Args:
-            size (int): The size of the square.
-        """
-        self.__size = size
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
-~                                                                                                                                                                                                     
--- INSERT --                                                                                                                                                                        1,1           Alli
+    pass
